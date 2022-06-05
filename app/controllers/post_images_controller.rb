f0aec4933fdb9cc58a53fc8ed79241f199a0d1ab
t-kiryu <104680945+t-kiryu@users.noreply.github.com>
2022-06-05 06:32:42 +0000

    [Start]4-18

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -23,7 +23,7 @@ class PostImagesController < ApplicationController
     @post_image.destroy
     redirect_to post_images_path
   end
-
+  
   private
   def post_image_params
     params.require(:post_image).permit(:shop_name, :caption)
