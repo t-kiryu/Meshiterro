a6e7db3cc61a8d3512f935f1829df22efbd7efad
t-kiryu <104680945+t-kiryu@users.noreply.github.com>
2022-06-06 03:58:45 +0000

    [Add]4-19

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,6 +5,7 @@ class PostImage < ApplicationRecord
   belongs_to :user
 
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   def get_image
     unless image.attached?
@@ -13,4 +14,9 @@ class PostImage < ApplicationRecord
     end
     image
   end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
