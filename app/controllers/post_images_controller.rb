f7963ce7c6dfc7f808cd0c7b5cc0ab155a9bc3e0
t-kiryu <104680945+t-kiryu@users.noreply.github.com>
2022-06-07 14:11:35 +0000

    [Start]4-24

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,4 +1,5 @@
 class PostImagesController < ApplicationController
+
   def new
     @post_image = PostImage.new
   end
@@ -6,12 +7,15 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
